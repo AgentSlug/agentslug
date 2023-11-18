bfc13a8f30a865a6224ce628dc29ed8216bca18d
Szymon Nowicki <sznowicki@eskalacja.com>
2023-11-18 16:34:20 +0100

    ci: project init

--- server/server.js
+++ server/server.js
@@ -1,5 +1,14 @@
-const main = () => {
+import express from 'express';
+import helmet from 'helmet';
 
-}
+
+const main = async () => {
+	const app = express();
+	app.use(helmet());
+
+	app.listen(process.env.APP_PORT, () => {
+		console.log(`Server is running on port ${process.env.APP_PORT}`);
+	});
+};
 
 main();
